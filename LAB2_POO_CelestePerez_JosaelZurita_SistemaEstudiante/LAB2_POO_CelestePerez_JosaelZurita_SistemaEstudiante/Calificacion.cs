4438c8afa820aee099309b6cfbe67a8c3edb1453
josael-droid <josael.zurita@utp.ac.pa>
2025-10-15 10:55:40 -0500

    Clase Estudiante y Calificacion

---
+++ LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/Calificacion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante
+{
+    internal class Calificacion
+    {
+    private Estudiante estudiante;
+        private Materia materia;
+        private double nota;
+
+        public Estudiante Estudiante
+        {
+            get => estudiante;
+            set => estudiante = value;
+        }
+
+        public Materia Materia
+        {
+            get => materia;
+            set => materia = value;
+        }
+
+        public double Nota
+        {
+            get => nota;
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentException("La nota debe estar entre 0 y 100");
+                nota = value;
+            }
+        }
+
+        public Calificacion(Estudiante estudiante, Materia materia, double nota)
+        {
+            Estudiante = estudiante;
+            Materia = materia;
+            Nota = nota;
+        }
+
+        public double CalcularPuntos()
+        {
+            return Nota * Materia.Creditos;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"[CALIFICACIÓN] Estudiante: {Estudiante.Nombre}, Materia: {Materia.Nombre}, Nota: {Nota}");
+        }
+    }
+
+}
+
---
+++ LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/Estudiante.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante
+{
+    internal class Estudiante
+    {
+        private string nombre;
+        private string id;
+        private string carrera;
+        private List<Calificacion> calificaciones = new List<Calificacion>();
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Carrera
+        {
+            get => carrera;
+            set => carrera = value;
+        }
+
+        public List<Calificacion> Calificaciones
+        {
+            get => calificaciones;
+        }
+
+        public Estudiante(string nombre, string id, string carrera)
+        {
+            Nombre = nombre;
+            Id = id;
+            Carrera = carrera;
+        }
+
+        public Estudiante() { }
+
+        public double CalcularPromedio()
+        {
+            if (Calificaciones.Count == 0) return 0;
+
+            double sumaPonderada = 0;
+            int totalCreditos = 0;
+            foreach (var c in Calificaciones)
+            {
+                sumaPonderada += c.Nota * c.Materia.Creditos;
+                totalCreditos += c.Materia.Creditos;
+            }
+            return sumaPonderada / totalCreditos;
+        }
+
+        public virtual void MostrarDatos()
+        {
+            Console.WriteLine($"[ESTUDIANTE] Nombre: {Nombre}, ID: {Id}, Carrera: {Carrera}, Promedio: {CalcularPromedio():0.00}");
+        }
+    }
+}
+
