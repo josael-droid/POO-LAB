d99383d1673efdfaf26cdcc4a220475cf5b257ad
CelestePerez1 <Celeste.perez@utp.ac.pa>
2025-10-15 11:04:24 -0500

    Clases Materia y Estudiantes becados

---
+++ LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/EstudianteBecado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante
+{
+    internal class EstudianteBecado : Estudiante
+    {
+        private double porcentajeBeca;
+
+        public double PorcentajeBeca
+        {
+            get => porcentajeBeca;
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentException("Porcentaje debe estar entre 0 y 100");
+                porcentajeBeca = value;
+            }
+        }
+
+        public EstudianteBecado(string nombre, string id, string carrera, double porcentajeBeca)
+            : base(nombre, id, carrera)
+        {
+            PorcentajeBeca = porcentajeBeca;
+        }
+
+        public double CalcularMatriculaConDescuento(double matriculaBase)
+        {
+            return matriculaBase * (1 - (PorcentajeBeca / 100.0));
+        }
+
+        public override void MostrarDatos()
+        {
+            Console.WriteLine($"[BECADO] Nombre: {Nombre}, ID: {Id}, Carrera: {Carrera}, Promedio: {CalcularPromedio():0.00}, Beca: {PorcentajeBeca}%");
+        }
+    }
+}
---
+++ LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante/Materia.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2_POO_CelestePerez_JosaelZurita_SistemaEstudiante
+{
+    internal class Materia
+    {
+        private string nombre;
+        private string codigo;
+        private int creditos;
+        private int cupos;
+        private int inscritos;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public string Codigo
+        {
+            get => codigo;
+            set => codigo = value;
+        }
+
+        public int Creditos
+        {
+            get => creditos;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Créditos debe ser > 0");
+                creditos = value;
+            }
+        }
+
+
+        public int Cupos
+        {
+            get => cupos;
+            set
+            {
+                if (value < 0) throw new ArgumentException("Cupos no puede ser negativo");
+                cupos = value;
+            }
+        }
+        public int Inscritos
+        {
+            get => inscritos;
+            set
+            {
+                if (value < 0 || value > Cupos) throw new ArgumentException("Inscritos inválido");
+                inscritos = value;
+            }
+        }
+        public Materia(string nombre, string codigo, int creditos, int cupos = 0, int inscritos = 0)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Creditos = creditos;
+            Cupos = cupos;
+            Inscritos = inscritos;
+        }
+
+        public int CalcularCargaSemanal(int horasPorCredito)
+        {
+            return Creditos * horasPorCredito;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"[MATERIA] Nombre: {Nombre}, Código: {Codigo}, Créditos: {Creditos}, Cupos: {Cupos}, Inscritos: {Inscritos}");
+        }
+
+
+    }
+}
